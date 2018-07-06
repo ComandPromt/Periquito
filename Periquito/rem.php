a4c1899d49b829e8f2a34380806c405bdab8ee1c
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-07-06 19:01:33 +0200

    Update rem.php

--- Periquito/rem.php
+++ Periquito/rem.php
@@ -10,7 +10,7 @@ if ($_SESSION['categoria'] != 9) {
     mover_imagenes("imagenes/");
     $imagenes = check_images("imagenes","jpg");
 } else {
-    $imagenes = check_images("imagenes/gif/","gif");
+    $imagenes = check_images("imagenes/gif","gif");
     gif_a_jpg($imagenes, "imagenes/gif/");
     redimensionarJPG(100, 125, "imagenes/gif/Thumb", true);
     $imagenes = showFiles("imagenes/gif","jpg");
@@ -18,14 +18,10 @@ if ($_SESSION['categoria'] != 9) {
 
 $hostbd = "192.168.1.2";
 $userbd = "root";
-$passbd = "pass";
+$passbd = "root";
 
-$numeros = array();
+
 $ids = array();
-
-if ($numeros[0] == NULL) {
-    $numero = 1;
-}
 
 if ($ids[0] == NULL) {
     $id = 1;
@@ -33,24 +29,21 @@ if ($ids[0] == NULL) {
 
 $conexion = mysqli_connect($hostbd, $userbd, $passbd, "folder");
 mysqli_select_db($conexion, 'folder');
-	
+	 $consulta = mysqli_query($conexion, "SELECT MAX(image_id)+1 FROM 4images_images ORDER BY image_id");
+	 while ($fila = mysqli_fetch_row($consulta)) {
+        $numero = $fila[0];
+    }
 for ($x = 0; $x < count($imagenes); $x++) {
     
-    $consulta = mysqli_query($conexion, "SELECT image_id FROM 4images_images ORDER BY image_id");
-
-    while ($fila = mysqli_fetch_row($consulta)) {
-        $numeros[] = $fila[0];
-    }
-
-    $numero = consecutivos($numeros);
     $thumb = substr($imagenes[$x], 0, -4) . "_Thumb.jpg";
     
     if (substr($imagenes[$x], -4, 1) == ".") {
         $consulta = "INSERT INTO 4images_images VALUES(" . $numero . "," . $_SESSION['categoria'] . ",1,'" . $_SESSION['nombre'] . "',DEFAULT,DEFAULT," . time() . ",DEFAULT,'" . $imagenes[$x] . "','" . $thumb . "','',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)";
-        mysqli_query($conexion, $consulta);
+  
+   mysqli_query($conexion, $consulta);
         $cadenas = caracteres($_SESSION['nombre']);
-        $consulta = mysqli_query($conexion, "SELECT word_id FROM 4images_wordlist where word_text='" . $_SESSION['nombre'] . "'");
-
+$consulta = mysqli_query($conexion, "SELECT word_id FROM 4images_wordlist where word_text='" . $_SESSION['nombre'] . "'");
+  
         while ($fila = mysqli_fetch_row($consulta)) {
             $id = $fila[0];
         }
@@ -61,14 +54,16 @@ for ($x = 0; $x < count($imagenes); $x++) {
 
         for ($y = 0; $y < count($cadenas); $y++) {
             $consulta = "INSERT INTO 4images_wordlist VALUES('$cadenas[$y]', $id)";
+			
             mysqli_query($conexion, $consulta);
             $consulta = "INSERT INTO 4images_wordmatch VALUES($numero,$id,1,0,0)";
-            mysqli_query($conexion, $consulta);
+			
+           mysqli_query($conexion, $consulta);
             $ids = array();
             $id = consecutivos($ids);
         }
     }
-    $numeros = array();
+    $numero++;
 }
 mysqli_close($conexion);
 print '<h1 id="salida" name="salida">' . $_SESSION['categoria'] . "</h1>";
