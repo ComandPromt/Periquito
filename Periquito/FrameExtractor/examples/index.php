5b9c6b1df86434a1b86b88397081d75ef8427334
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-09-11 14:00:11 +0200

    Update index.php

--- Periquito/FrameExtractor/examples/index.php
+++ Periquito/FrameExtractor/examples/index.php
@@ -19,27 +19,58 @@ if(count(check_images("output","jpg"))>=1){
 }
 
 else{
-	$numimmp4=check_images("video","mp4");
-	$numimpg=check_images("video","mpg");
-	$numiavi=check_images("video","avi");
-	$numimkv=check_images("video","mkv");
-$videos=array();
-if(count($numimmp4)==1){
-	$videos[0]=$numimmp4[0];
+	$numimmp4=check_images("./","mp4");
+	$numiflv=check_images("./","flv");
+	$numim3u8=check_images("./","m3u8");
+	$numimkv=check_images("./","mkv");
+	$numimts=check_images("./","ts");
+	$numiavi=check_images("./","avi");
+	$numi3gp=check_images("./","3gp");
+	$numimov=check_images("./","mov");
+	$numiwmv=check_images("./","wmv");
+	$videos=array();
 
-}	
-if(count($numiavi)==1 && $videos[0]==null){
-	$videos[0]=$numiavi[0];
-}
-if(count($numimpg)==1 && $videos[0]==null){
-	$videos[0]=$numimpg[0];
-}
-if(count($numimkv)==1 && $videos[0]==null){
-	$videos[0]=$numimkv[0];
-}
+	if(count($numimmp4)==1){
+		$videos[0]=$numimmp4[0];
+	
+	}
+	
+	if(count($numiflv)==1 && $videos[0]==null){
+		$videos[0]=$numiflv[0];
+	}
+	
+	if(count($numim3u8)==1 && $videos[0]==null){
+		$videos[0]=$numim3u8[0];
+	}
+	
+	if(count($numimkv)==1 && $videos[0]==null){
+		$videos[0]=$numimkv[0];
+	}
+	
+	if(count($numimts)==1 && $videos[0]==null){
+		$videos[0]=$numimts[0];
+	}
+		
+	if(count($numiavi)==1 && $videos[0]==null){
+		$videos[0]=$numiavi[0];
+	}
+	
+	if(count($numi3gp)==1 && $videos[0]==null){
+		$videos[0]=$numi3gp[0];
+	}
+	
+	if(count($numimov)==1 && $videos[0]==null){
+		$videos[0]=$numimov[0];
+	}
+	
+	if(count($numiwmv)==1 && $videos[0]==null){
+		$videos[0]=$numiwmv[0];
+	}
+	
 	if(count($videos)==0){
 			print '<h1 name="salida">No tienes videos</h1>';
 	}
+	
 	else{
 		date_default_timezone_set('Europe/Madrid');
 		include_once 'includes/bootstrap.php';
