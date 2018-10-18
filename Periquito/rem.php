c81453a14e8a06e5cc00b9c2b73fa69ca08c4793
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-18 19:22:08 +0200

    Update rem.php

--- Periquito/rem.php
+++ Periquito/rem.php
@@ -11,7 +11,7 @@ if ($_SESSION['categoria'] != 9) {
     $imagenes = check_images("imagenes/gif","gif");
     gif_a_jpg($imagenes, "imagenes/gif/");
     redimensionarJPG(100, 125, "imagenes/gif/Thumb", true);
-    $imagenes = check_images("imagenes/gif","jpg");
+    $imagenes = check_images("imagenes/gif","gif");
 }
 $hostbd = "192.168.1.2";
 $userbd = "root";
@@ -20,22 +20,22 @@ $ids = array();
 if ($ids[0] == NULL) {
     $id = 1;
 }
-$conexion = mysqli_connect($hostbd, $userbd, $passbd, "folder");
-mysqli_select_db($conexion, 'folder');
+$conexion = mysqli_connect($hostbd, $userbd, $passbd, "hoopfetish");
+mysqli_select_db($conexion, 'hoopfetish');
 	 $consulta = mysqli_query($conexion, "SELECT MAX(image_id)+1 FROM 4images_images ORDER BY image_id");
 	 while ($fila = mysqli_fetch_row($consulta)) {
         $numero = $fila[0];
     }
+
 for ($x = 0; $x < count($imagenes); $x++) {
     
     $thumb = substr($imagenes[$x], 0, -4) . "_Thumb.jpg";
     
     if (substr($imagenes[$x], -4, 1) == ".") {
         $consulta = "INSERT INTO 4images_images VALUES(" . $numero . "," . $_SESSION['categoria'] . ",1,'" . $_SESSION['nombre'] . "',DEFAULT,DEFAULT," . time() . ",DEFAULT,'" . $imagenes[$x] . "','" . $thumb . "','',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)";
-  
-   mysqli_query($conexion, $consulta);
+		mysqli_query($conexion, $consulta);
         $cadenas = caracteres($_SESSION['nombre']);
-$consulta = mysqli_query($conexion, "SELECT word_id FROM 4images_wordlist where word_text='" . $_SESSION['nombre'] . "'");
+		$consulta = mysqli_query($conexion, "SELECT word_id FROM 4images_wordlist where word_text='" . $_SESSION['nombre'] . "'");
   
         while ($fila = mysqli_fetch_row($consulta)) {
             $id = $fila[0];
@@ -46,11 +46,9 @@ $consulta = mysqli_query($conexion, "SELECT word_id FROM 4images_wordlist where 
         }
         for ($y = 0; $y < count($cadenas); $y++) {
             $consulta = "INSERT INTO 4images_wordlist VALUES('$cadenas[$y]', $id)";
-			
             mysqli_query($conexion, $consulta);
             $consulta = "INSERT INTO 4images_wordmatch VALUES($numero,$id,1,0,0)";
-			
-           mysqli_query($conexion, $consulta);
+			mysqli_query($conexion, $consulta);
             $ids = array();
             $id = consecutivos($ids);
         }
