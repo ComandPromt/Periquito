22c1be73e65af08c55d6c43d68ec751e94d3c656
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-28 01:45:16 +0200

    Add files via upload

---
+++ Java/src/main/java/periquito/DriverSeleniumChrome.java
@@ -0,0 +1,10 @@
+package periquito;
+
+public class DriverSeleniumChrome extends DriverSelenium {
+
+	public DriverSeleniumChrome() {
+
+		// Indicamos la ruta del driver que levantara chrome
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+	}
+}
