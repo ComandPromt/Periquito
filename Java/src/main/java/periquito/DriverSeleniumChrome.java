4be11b005feacb331f7c6145a030543a28ccfcb7
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-11-09 00:02:27 +0100

    Update DriverSeleniumChrome.java

--- Java/src/main/java/periquito/DriverSeleniumChrome.java
+++ Java/src/main/java/periquito/DriverSeleniumChrome.java
@@ -3,8 +3,7 @@ package periquito;
 public class DriverSeleniumChrome extends DriverSelenium {
 
 	public DriverSeleniumChrome() {
-
 		// Indicamos la ruta del driver que levantara chrome
-		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	}
 }
