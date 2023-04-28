9649091f64b889c05c366049a903826c001cca35
ComandPromt <smr2gocar@gmail.com>
2023-04-28 21:03:53 +0200

    fix v 4

--- Dependencies/Windows/install.bat
+++ Dependencies/Windows/install.bat
@@ -1,10 +1,27 @@
 @echo off
+pip install --upgrade pip
 pip install imageio
 pip install tk
 pip install pathlib
 pip install Image
+pip install scikit-image
+pip install opencv-python
+pip install matplotlib
+pip install torch
+pip install IPython
+pip install skimage
+pip install wxpython
+pip install moviepy
 pip3 install imageio
 pip3 install tk
 pip3 install pathlib
 pip3 install Image
+pip3 install matplotlib
+pip3 install opencv-python
+pip3 install torch
+pip3 install IPython
+pip3 install skimage
+pip3 install wxpython
+pip3 install moviepy
+pip3 install scikit-image
 exit
